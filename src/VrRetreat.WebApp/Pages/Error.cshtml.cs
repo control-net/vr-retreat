ec7310fbe9af7a6ce9d4515985b52fc99bafeee3
petrspelos <petrspelos@users.noreply.github.com>
2021-11-27 15:31:37 +0100

    Switch from React to MVC

--- src/VrRetreat.WebApp/Pages/Error.cshtml.cs
+++ src/VrRetreat.WebApp/Pages/Error.cshtml.cs
@@ -5,18 +5,19 @@ using System.Diagnostics;
 namespace VrRetreat.WebApp.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
         private readonly ILogger<ErrorModel> _logger;
 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
-
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         public void OnGet()
         {
