1e8ce6e5f0ac8ce38c8b8ad2733b0989c6f9a563
petrspelos <petrspelos@users.noreply.github.com>
2021-12-05 15:47:35 +0100

    Upgrade VRChat API wrapper version

--- src/VrRetreat.Tests/VrcApiTests.cs
+++ src/VrRetreat.Tests/VrcApiTests.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using VrRetreat.Core;
 using VrRetreat.Infrastructure;
 using Xunit;
 
-namespace VrRetreat.Tests
+namespace VrRetreat.Tests;
+
+public class VrcApiTests
 {
-    public class VrcApiTests
+    private readonly IVrChat _sut;
+
+    public VrcApiTests()
     {
-        private readonly IVrChat _sut;
+        _sut = new VrChat(JsonConfiguration.FromFile("TestConfig.json"));
+    }
 
-        public VrcApiTests()
-        {
-            _sut = new VrChat(JsonConfiguration.FromFile("TestConfig.json"));
-        }
+    [Theory(Skip = "Officially, we're not supposed to call the API more than once per 60 seconds!")]
+    [InlineData("Spelos", true)]
+    [InlineData("7cb3c694-43df-4923-9a2e-11f6c316e206", false)]
+    public async Task ExistingUser_ShouldReturnExists(string username, bool expected)
+    {
+        await _sut.InitializeAsync();
 
-        [Theory(Skip = "Officially, we're not supposed to call the API more than once per 60 seconds!")]
-        [InlineData("Spelos", true)]
-        [InlineData("7cb3c694-43df-4923-9a2e-11f6c316e206", false)]
-        public async Task ExistingUser_ShouldReturnExists(string username, bool expected)
-        {
-            await _sut.InitializeAsync();
+        var actual = await _sut.GetPlayerExistsAsync(username);
 
-            var actual = await _sut.GetPlayerExistsAsync(username);
-
-            Assert.Equal(expected, actual);
-        }
+        Assert.Equal(expected, actual);
     }
 }
