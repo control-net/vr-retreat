b0bac4ffd4809faa3df5b35b5103d58edea0d724
petrspelos <petrspelos@users.noreply.github.com>
2021-12-20 23:29:01 +0100

    Setup Dashboard view for UI development

--- src/VrRetreat.WebApp/Controllers/HomeController.cs
+++ src/VrRetreat.WebApp/Controllers/HomeController.cs
@@ -13,9 +13,44 @@ namespace VrRetreat.WebApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = new DashboardViewModel
+            {
+                CurrentUser = new()
+                {
+                    Username = "voxie",
+                    AvatarUrl = "https://api.vrchat.cloud/api/1/image/file_bcad5b12-95e8-4d3b-9393-4941dd9c9567/1/1280",
+                    LastVrChatLogin = DateTime.Now.AddDays(-2).AddHours(-10),
+                    Failed = false
+                },
+                FollowedPeople = new []
+                {
+                    new UserDashboardModel()
+                    {
+                        Username = "CallMeSalad",
+                        AvatarUrl = "https://api.vrchat.cloud/api/1/image/file_6eec5ee9-8096-4b8f-9410-54045c0b8221/2/256",
+                        LastVrChatLogin = DateTime.Now.AddDays(-4),
+                        Failed = false
+                    },
+                    new UserDashboardModel()
+                    {
+                        Username = "Pusheenmon",
+                        AvatarUrl = "https://api.vrchat.cloud/api/1/image/file_ac8dba90-5c83-49e1-b84c-bfd5aeb4bd99/1/256",
+                        LastVrChatLogin = DateTime.Now.AddDays(-1).AddHours(-3),
+                        Failed = false
+                    },
+                    new UserDashboardModel()
+                    {
+                        Username = "273BeLow",
+                        AvatarUrl = "https://api.vrchat.cloud/api/1/image/file_351dcee8-0aec-40ae-a255-9faf16dfdda4/1/256",
+                        LastVrChatLogin = DateTime.Now.AddHours(-10),
+                        Failed = true
+                    }
+                }
+            };
+
+            return View("Dashboard", model);
         }
 
         public IActionResult Privacy()
---
+++ src/VrRetreat.WebApp/Models/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace VrRetreat.WebApp.Models;
+
+public class DashboardViewModel
+{
+    public UserDashboardModel? CurrentUser { get; set; }
+    public IEnumerable<UserDashboardModel> FollowedPeople { get; set; } = Array.Empty<UserDashboardModel>();
+}
+
+public class UserDashboardModel
+{
+    public string Username { get; set; } = string.Empty;
+    public string AvatarUrl { get; set; } = string.Empty;
+    public DateTime LastVrChatLogin { get; set; }
+    public TimeSpan OfflineDuration => DateTime.UtcNow - LastVrChatLogin;
+    public bool Failed { get; set; }
+}
