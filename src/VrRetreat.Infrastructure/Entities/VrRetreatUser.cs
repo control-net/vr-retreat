4facc4b8add48b107e8c6f33d7c452a2106ae41a
petrspelos <petrspelos@users.noreply.github.com>
2021-12-22 16:56:47 +0100

    Add Verification Properties to VrRetreatUser

--- src/VrRetreat.Infrastructure/Entities/VrRetreatUser.cs
+++ src/VrRetreat.Infrastructure/Entities/VrRetreatUser.cs
@@ -10,4 +10,8 @@ public class VrRetreatUser : IdentityUser, IVrRetreatUser
     public string VrChatAvatarUrl { get; set; } = string.Empty;
     public DateTime? VrChatLastLogin { get; set; }
     public bool FailedChallenge { get; set; }
+    public string BioCode { get; set; } = string.Empty;
+    public bool IsVrChatAccountLinked { get; set; }
+    public DateTime? LastFriendRequestSent { get; set; }
+    public DateTime? LastBioCheck { get; set; }
 }
---
+++ src/VrRetreat.WebApp/Data/Migrations/20211222155527_AddVerificationProperties.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VrRetreat.WebApp.Data.Migrations
+{
+    public partial class AddVerificationProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BioCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVrChatAccountLinked",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastBioCheck",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastFriendRequestSent",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BioCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsVrChatAccountLinked",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastBioCheck",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastFriendRequestSent",
+                table: "AspNetUsers");
+        }
+    }
+}
