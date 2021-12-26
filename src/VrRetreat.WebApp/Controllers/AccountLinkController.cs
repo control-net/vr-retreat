d25d8103ae8064b256958e88024776890b6b02f3
petrspelos <petrspelos@users.noreply.github.com>
2021-12-26 14:12:26 +0100

    Extract VRC Account Linking into a new controller

---
+++ src/VrRetreat.WebApp/Controllers/AccountLinkController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VrRetreat.Core.Boundaries.BioCodeVerification;
+using VrRetreat.Core.Boundaries.Infrastructure;
+using VrRetreat.Core.Boundaries.VrChatAccountClaim;
+using VrRetreat.Core.Boundaries.VrChatVerifyFriendStatus;
+using VrRetreat.Infrastructure.Entities;
+using VrRetreat.WebApp.Models;
+using VrRetreat.WebApp.Presenters;
+
+namespace VrRetreat.WebApp.Controllers;
+
+[Authorize]
+public class AccountLinkController : Controller
+{
+    private readonly UserManager<VrRetreatUser> _userManager;
+    private readonly IUserRepository _userRepository;
+
+    private readonly IVrChatAccountClaimUseCase _accountClaimUseCase;
+    private readonly VrChatAccountClaimPresenter _accountClaimPresenter;
+
+    private readonly IVrChatVerifyFriendStatusUseCase _friendStatusUseCase;
+    private readonly VrChatVerifyFriendStatusPresenter _friendStatusPresenter;
+
+    private readonly IBioCodeVerificationUseCase _bioVerificationUseCase;
+    private readonly BioCodeVerificationPresenter _bioVerificationPresenter;
+
+    public AccountLinkController(UserManager<VrRetreatUser> userManager, IUserRepository userRepository,
+            IVrChatAccountClaimUseCase accountClaimUseCase, VrChatAccountClaimPresenter accountClaimPresenter,
+            IVrChatVerifyFriendStatusUseCase friendStatusUseCase, VrChatVerifyFriendStatusPresenter friendStatusPresenter,
+            IBioCodeVerificationUseCase bioVerificationUseCase, BioCodeVerificationPresenter bioVerificationPresenter)
+    {
+        _userManager = userManager;
+        _userRepository = userRepository;
+
+        _accountClaimUseCase = accountClaimUseCase;
+        _accountClaimPresenter = accountClaimPresenter;
+
+        _friendStatusUseCase = friendStatusUseCase;
+        _friendStatusPresenter = friendStatusPresenter;
+
+        _bioVerificationUseCase = bioVerificationUseCase;
+        _bioVerificationPresenter = bioVerificationPresenter;
+    }
+
+    public IActionResult ClaimVrChatName() => View(new VrChatNameClaimModel());
+
+    [HttpPost]
+    public async Task<IActionResult> ClaimVrChatName(VrChatNameClaimModel model)
+    {
+        _accountClaimPresenter.ModelState = ModelState;
+
+        await _accountClaimUseCase.ExecuteAsync(new(User.Identity?.Name!, model.VrChatName));
+
+        if (_accountClaimPresenter.Result is not null)
+            return _accountClaimPresenter.Result;
+
+        if (_accountClaimPresenter.Success)
+            return RedirectToAction(nameof(VrChatFriendRequestValidation));
+
+        return View(model);
+    }
+
+    public IActionResult VrChatFriendRequestValidation() => View(new VrChatFriendRequestModel());
+
+    [HttpPost]
+    public async Task<IActionResult> VrChatFriendRequestValidation(VrChatFriendRequestModel model)
+    {
+        _friendStatusPresenter.ModelState = ModelState;
+
+        await _friendStatusUseCase.ExecuteAsync(new(User.Identity?.Name!));
+
+        if (_friendStatusPresenter.Result is not null)
+            return _friendStatusPresenter.Result;
+
+        if (_friendStatusPresenter.Success)
+            return RedirectToAction(nameof(VrChatBioCodeValidation));
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> VrChatBioCodeValidation()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user is null)
+            return RedirectToAction("Index", "Home");
+
+        // NOTE(Peter): It would be better to also display a message after this redirect
+        if (user.BioCode is null)
+            return RedirectToAction(nameof(ClaimVrChatName));
+
+        return View(new VrChatBioCodeValidationModel { BioCode = user.BioCode });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> VrChatBioCodeValidation(VrChatBioCodeValidationModel model)
+    {
+        _bioVerificationPresenter.ModelState = ModelState;
+
+        await _bioVerificationUseCase.ExecuteAsync(new(User.Identity?.Name!));
+
+        if (_bioVerificationPresenter.Result is not null)
+            return _bioVerificationPresenter.Result;
+
+        if (_bioVerificationPresenter.Success)
+            return RedirectToAction("Index", "Home");
+
+        return View(model);
+    }
+}
--- src/VrRetreat.WebApp/Controllers/HomeController.cs
+++ src/VrRetreat.WebApp/Controllers/HomeController.cs
@@ -1,159 +1,61 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using VrRetreat.Core.Boundaries.BioCodeVerification;
 using VrRetreat.Core.Boundaries.Infrastructure;
-using VrRetreat.Core.Boundaries.VrChatAccountClaim;
-using VrRetreat.Core.Boundaries.VrChatVerifyFriendStatus;
 using VrRetreat.Infrastructure.Entities;
 using VrRetreat.WebApp.Models;
-using VrRetreat.WebApp.Presenters;
 
-namespace VrRetreat.WebApp.Controllers
+namespace VrRetreat.WebApp.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly SignInManager<VrRetreatUser> _signInManager;
+    private readonly UserManager<VrRetreatUser> _userManager;
+    private readonly IUserRepository _userRepository;
+
+    public HomeController(SignInManager<VrRetreatUser> signInManager, UserManager<VrRetreatUser> userManager, IUserRepository userRepository)
     {
-        private readonly SignInManager<VrRetreatUser> _signInManager;
-        private readonly IUserRepository _userRepository;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _userRepository = userRepository;
+    }
 
-        private readonly IVrChatAccountClaimUseCase _accountClaimUseCase;
-        private readonly VrChatAccountClaimPresenter _accountClaimPresenter;
+    public async Task<IActionResult> Index()
+    {
+        if (!_signInManager.IsSignedIn(User))
+            return View();
 
-        private readonly IVrChatVerifyFriendStatusUseCase _friendStatusUseCase;
-        private readonly VrChatVerifyFriendStatusPresenter _friendStatusPresenter;
-
-        private readonly IBioCodeVerificationUseCase _bioVerificationUseCase;
-        private readonly BioCodeVerificationPresenter _bioVerificationPresenter;
-
-        public HomeController(SignInManager<VrRetreatUser> signInManager, IUserRepository userRepository,
-            IVrChatAccountClaimUseCase accountClaimUseCase, VrChatAccountClaimPresenter accountClaimPresenter,
-            IVrChatVerifyFriendStatusUseCase friendStatusUseCase, VrChatVerifyFriendStatusPresenter friendStatusPresenter,
-            IBioCodeVerificationUseCase bioVerificationUseCase, BioCodeVerificationPresenter bioVerificationPresenter)
+        if (await _userRepository.HasLinkedAccountByUsername(User.Identity?.Name ?? string.Empty))
         {
-            _userRepository = userRepository;
-            _signInManager = signInManager;
-
-            _accountClaimUseCase = accountClaimUseCase;
-            _accountClaimPresenter = accountClaimPresenter;
-
-            _friendStatusUseCase = friendStatusUseCase;
-            _friendStatusPresenter = friendStatusPresenter;
-
-            _bioVerificationUseCase = bioVerificationUseCase;
-            _bioVerificationPresenter = bioVerificationPresenter;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            if (!_signInManager.IsSignedIn(User))
-                return View();
-
-            if (await _userRepository.HasLinkedAccountByUsername(User.Identity?.Name ?? string.Empty))
-            {
-                var user = await _userRepository.GetUserByUsernameAsync(User.Identity.Name);
-                // NOTE(Peter): Once Dashboard is finished, we'll construct the model here.
-                return View("Dashboard", new DashboardViewModel
-                {
-                    CurrentUser = new()
-                    {
-                        AvatarUrl = user.VrChatAvatarUrl,
-                        Failed = user.FailedChallenge,
-                        Username = user.VrChatName
-                    }
-                });
-            }
-
-            return View("AccountLinking");
-        }
-
-        [Authorize]
-        public IActionResult ClaimVrChatName()
-        {
-            return View(new VrChatNameClaimModel());
-        }
-
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> ClaimVrChatName(VrChatNameClaimModel model)
-        {
-            _accountClaimPresenter.ModelState = ModelState;
-
-            await _accountClaimUseCase.ExecuteAsync(new(User.Identity.Name, model.VrChatName));
-
-            if(_accountClaimPresenter.Result is not null)
-                return _accountClaimPresenter.Result;
-
-            if(_accountClaimPresenter.Success)
-                return RedirectToAction(nameof(VrChatFriendRequestValidation));
-
-            return View(model);
-        }
-
-        [Authorize]
-        public IActionResult VrChatFriendRequestValidation()
-        {
-            return View(new VrChatFriendRequestModel());
-        }
-
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> VrChatFriendRequestValidation(VrChatFriendRequestModel model)
-        {
-            _friendStatusPresenter.ModelState = ModelState;
-
-            await _friendStatusUseCase.ExecuteAsync(new(User.Identity.Name));
-
-            if (_friendStatusPresenter.Result is not null)
-                return _friendStatusPresenter.Result;
-
-            if (_friendStatusPresenter.Success)
-                return RedirectToAction(nameof(VrChatBioCodeValidation));
-
-            return View(model);
-        }
-
-        [Authorize]
-        public async Task<IActionResult> VrChatBioCodeValidation()
-        {
-            // TODO(Peter): improve null checking
-            var user = await _userRepository.GetUserByUsernameAsync(User.Identity.Name);
-            return View(new VrChatBioCodeValidationModel()
-            {
-                BioCode = user?.BioCode
-            });
-        }
-
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> VrChatBioCodeValidation(VrChatBioCodeValidationModel model)
-        {
-            _bioVerificationPresenter.ModelState = ModelState;
-
-            await _bioVerificationUseCase.ExecuteAsync(new(User.Identity.Name));
-
-            if (_bioVerificationPresenter.Result is not null)
-                return _bioVerificationPresenter.Result;
-
-            if (_bioVerificationPresenter.Success)
-                return RedirectToAction(nameof(Index));
-
-            return View(model);
-        }
-
-        public IActionResult Dashboard()
-        {
-            var model = new DashboardViewModel
+            var user = await _userManager.GetUserAsync(User);
+            // NOTE(Peter): Once Dashboard is finished, we'll construct the model here.
+            return View("Dashboard", new DashboardViewModel
             {
                 CurrentUser = new()
                 {
-                    Username = "voxie",
-                    AvatarUrl = "https://api.vrchat.cloud/api/1/image/file_bcad5b12-95e8-4d3b-9393-4941dd9c9567/1/1280",
-                    LastVrChatLogin = DateTime.Now.AddDays(-2).AddHours(-10),
-                    Failed = false
-                },
-                FollowedPeople = new[]
-                {
+                    AvatarUrl = user.VrChatAvatarUrl,
+                    Failed = user.FailedChallenge,
+                    Username = user.VrChatName
+                }
+            });
+        }
+
+        return View("UnlinkedAccount");
+    }
+
+    public IActionResult Dashboard()
+    {
+        var model = new DashboardViewModel
+        {
+            CurrentUser = new()
+            {
+                Username = "voxie",
+                AvatarUrl = "https://api.vrchat.cloud/api/1/image/file_bcad5b12-95e8-4d3b-9393-4941dd9c9567/1/1280",
+                LastVrChatLogin = DateTime.Now.AddDays(-2).AddHours(-10),
+                Failed = false
+            },
+            FollowedPeople = new[]
+            {
                     new UserDashboardModel()
                     {
                         Username = "CallMeSalad",
@@ -176,15 +78,14 @@ namespace VrRetreat.WebApp.Controllers
                         Failed = true
                     }
                 }
-            };
+        };
 
-            return View("Dashboard", model);
-        }
+        return View("Dashboard", model);
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
