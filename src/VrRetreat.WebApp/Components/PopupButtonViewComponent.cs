e8d900ac04151b70eaff5e6b08d414aed7e8b7f0
Alex <sepp.amsl@protonmail.com>
2021-12-29 18:30:26 +0100

    Implement Confirmation Dialogues
    closes #24

---
+++ src/VrRetreat.WebApp/Components/PopupButtonViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VrRetreat.WebApp.Models.Components;
+
+namespace VrRetreat.WebApp.Components;
+
+public class PopupButtonViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(
+        string modalTitle,
+        string modalDescription,
+        string popupButtonText,
+        string popupId,
+        string confirmButtonAction,
+        string popupButtonClass = "btn btn-danger",
+        string confirmButtonClass = "btn btn-danger",
+        string confirmButtonText = "Confirm")
+    {
+        var viewModel = new PopupButtonViewModel
+        {
+            Title = modalTitle,
+            Description = modalDescription,
+
+            PopupId = popupId,
+            PopupButtonClass = popupButtonClass,
+            PopupButtonText = popupButtonText,
+
+            ConfirmButtonAction = confirmButtonAction,
+            ConfirmButtonClass = confirmButtonClass,
+            ConfirmButtonText = confirmButtonText
+        };
+        return View(viewModel);
+    }
+
+
+}
+
---
+++ src/VrRetreat.WebApp/Models/Components/PopupButtonViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace VrRetreat.WebApp.Models.Components;
+
+public class PopupButtonViewModel
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public string PopupButtonClass { get; set; } = string.Empty;
+    public string PopupButtonText { get; set; } = string.Empty;
+    public string PopupId { get; set; } = string.Empty;
+
+    public string ConfirmButtonAction { get; set; } = string.Empty;
+    public string ConfirmButtonClass { get; set; } = string.Empty;
+    public string ConfirmButtonText { get; set; } = string.Empty;
+}
+
